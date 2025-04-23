d384d066f8fefaeda25aa7b4fdf975e2b859a3a5
AlexSanches1 <sanches2026@gmail.com>
2025-04-23 11:34:26 -0400

    Auto-Update the Subcommands for Nix WIP

--- nix/flake.nix
+++ nix/flake.nix
@@ -21,22 +21,22 @@
       };
 
       urls = {
-        "x86_64-linux" = {
+        x86_64-linux = {
           asimov         = "https://github.com/asimov-platform/asimov-cli/releases/download/${versions.asimov}/asimov-linux-x86-gnu.gz";
           asimov-module  = "https://github.com/asimov-platform/asimov-module-cli/releases/download/${versions.asimov-module}/asimov-module-cli-linux-x86-gnu.gz";
           asimov-dataset = "https://github.com/asimov-platform/asimov-dataset-cli/releases/download/${versions.asimov-dataset}/asimov-dataset-cli-linux-x86-gnu.gz";
         };
-        "aarch64-linux" = {
+        aarch64-linux = {
           asimov         = "https://github.com/asimov-platform/asimov-cli/releases/download/${versions.asimov}/asimov-linux-arm-gnu.gz";
           asimov-module  = "https://github.com/asimov-platform/asimov-module-cli/releases/download/${versions.asimov-module}/asimov-module-cli-linux-arm-gnu.gz";
           asimov-dataset = "https://github.com/asimov-platform/asimov-dataset-cli/releases/download/${versions.asimov-dataset}/asimov-dataset-cli-linux-arm-gnu.gz";
         };
-        "x86_64-darwin" = {
+        x86_64-darwin = {
           asimov         = "https://github.com/asimov-platform/asimov-cli/releases/download/${versions.asimov}/asimov-macos-x86.gz";
           asimov-module  = "https://github.com/asimov-platform/asimov-module-cli/releases/download/${versions.asimov-module}/asimov-module-cli-macos-x86.gz";
           asimov-dataset = "https://github.com/asimov-platform/asimov-dataset-cli/releases/download/${versions.asimov-dataset}/asimov-dataset-cli-macos-x86.gz";
         };
-        "aarch64-darwin" = {
+        aarch64-darwin = {
           asimov         = "https://github.com/asimov-platform/asimov-cli/releases/download/${versions.asimov}/asimov-macos-arm.gz";
           asimov-module  = "https://github.com/asimov-platform/asimov-module-cli/releases/download/${versions.asimov-module}/asimov-module-cli-macos-arm.gz";
           asimov-dataset = "https://github.com/asimov-platform/asimov-dataset-cli/releases/download/${versions.asimov-dataset}/asimov-dataset-cli-macos-arm.gz";
@@ -44,22 +44,22 @@
       };
 
       hashes = {
-        "x86_64-linux" = { # 6b6e54fd490e036d864158de7d4b19a248aaf64c926ff9d3daa9db0f463d6f9c
+        x86_64-linux = { # 6b6e54fd490e036d864158de7d4b19a248aaf64c926ff9d3daa9db0f463d6f9c
           asimov         = "d8cb3ad98499d42ec4f89c6669264935a41b0583f6e3995b7e1382b73b654af6";
           asimov-module  = "d8cb3ad98499d42ec4f89c6669264935a41b0583f6e3995b7e1382b73b654af6";
           asimov-dataset = "abbcf6612278381d079e85b658cced97b47f71652437fa39e6eb8dc2423d6d6d";
         };
-        "aarch64-linux" = { # ef21144725eeccd0618357f0bb4936d1653a8968bfa8b530fa2490685470dc33
+        aarch64-linux = { # ef21144725eeccd0618357f0bb4936d1653a8968bfa8b530fa2490685470dc33
           asimov         = "83deb2d62df03a24e5f3965a9a137efe423714c9d852763f6223ff4dc96d33c0";
           asimov-module  = "b90bc4cda8f648b5d0f7863f141c9a314b4410edf40fd7b0f5a20c4c9aa01b30";
           asimov-dataset = "83deb2d62df03a24e5f3965a9a137efe423714c9d852763f6223ff4dc96d33c0";
         };
-        "x86_64-darwin" = {
+        x86_64-darwin = {
           asimov         = "eaaa532d63de98905367816b5ec6fad87815d2911d6d57bf7ddeec446f36bec5";
           asimov-module  = "18b26b321279923109b5dc81d7c8cb3cb5db7179a5db6e27601849add9195c37";
           asimov-dataset = "ea11d213dc01c99171e456de94958acc37abe84992b1c234b48e75a85ad6497d";
         };
-        "aarch64-darwin" = {
+        aarch64-darwin = {
           asimov         = "e67ec61334647909fa1d405498ca42a6e7a9a96471a435ec5bbb7dc784c08ce4";
           asimov-module  = "39d3c86b017c0b51a263f946f44d398161f44920e3b224e8c718cca3f7cbd57d";
           asimov-dataset = "cf4f4b1058db1f5238df985b3958e596c6eea84c3f92f0d8f0f5a489174eefbf";
